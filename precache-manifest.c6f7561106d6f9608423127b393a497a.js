cfaeaefdf9882347c467aea63024519d10ac62e3
kimute <jkdnekki@icloud.com>
2020-09-01 00:13:10 +0900

    Updates

---
+++ precache-manifest.c6f7561106d6f9608423127b393a497a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c40c577e3ef2de6581348d979aaf49f",
+    "url": "/Frontend_test_react/index.html"
+  },
+  {
+    "revision": "63d4fe1d33525b1a51bc",
+    "url": "/Frontend_test_react/static/css/main.ea9663c9.chunk.css"
+  },
+  {
+    "revision": "1daa6e99c2d1da191b73",
+    "url": "/Frontend_test_react/static/js/2.4c009d2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Frontend_test_react/static/js/2.4c009d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d4fe1d33525b1a51bc",
+    "url": "/Frontend_test_react/static/js/main.0da25e84.chunk.js"
+  },
+  {
+    "revision": "2e5159183cd5e0f9b056",
+    "url": "/Frontend_test_react/static/js/runtime-main.8f1cfbbb.js"
+  }
+]);
